b51c9ce0d2216eedbeae237fac20039a80f9e39e
rxxi00 <skyd65929@gmail.com>
2025-06-08 19:12:31 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,121 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-timothy-oldfield-luufnhochru-unsplash-1",
+      "name": "timothy-oldfield-luufnHoChRU-unsplash (1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.5018154260862246,
+        "pitch": -0.0050060414100947526,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5155376816109545,
+          "pitch": -0.0021755257795224026,
+          "rotation": 0,
+          "target": "1-full-seamless-spherical-hdri-panorama-360-degrees-angle-view-on-wooden-pier-near-lake-in-evening-in-equirectangular-projection-with-zenith-ready-vr-ar-virtual-reality-content-photo"
+        },
+        {
+          "yaw": 0.4857754446682865,
+          "pitch": -0.11911509014172594,
+          "rotation": 0,
+          "target": "2-360_f_322882600_y6jbonld7yldrru5lfqreuq8yuwasfgg"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-full-seamless-spherical-hdri-panorama-360-degrees-angle-view-on-wooden-pier-near-lake-in-evening-in-equirectangular-projection-with-zenith-ready-vr-ar-virtual-reality-content-photo",
+      "name": "full-seamless-spherical-hdri-panorama-360-degrees-angle-view-on-wooden-pier-near-lake-in-evening-in-equirectangular-projection-with-zenith-ready-vr-ar-virtual-reality-content-photo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 490,
+      "initialViewParameters": {
+        "yaw": -1.3321547281079091,
+        "pitch": 0.027533227755517586,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3321547281079091,
+          "pitch": 0.027533227755517586,
+          "rotation": 0,
+          "target": "1-full-seamless-spherical-hdri-panorama-360-degrees-angle-view-on-wooden-pier-near-lake-in-evening-in-equirectangular-projection-with-zenith-ready-vr-ar-virtual-reality-content-photo"
+        },
+        {
+          "yaw": -0.5239140714667911,
+          "pitch": -0.03582127035173244,
+          "rotation": 0,
+          "target": "2-360_f_322882600_y6jbonld7yldrru5lfqreuq8yuwasfgg"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-360_f_322882600_y6jbonld7yldrru5lfqreuq8yuwasfgg",
+      "name": "360_F_322882600_y6JbONLD7YLdRrU5LFQReuq8YUwasfgg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 180,
+      "initialViewParameters": {
+        "yaw": 1.062427670501064,
+        "pitch": 1.5707963267948966,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.04044721490700809,
+          "pitch": 0.5755321550022874,
+          "rotation": 0,
+          "target": "1-full-seamless-spherical-hdri-panorama-360-degrees-angle-view-on-wooden-pier-near-lake-in-evening-in-equirectangular-projection-with-zenith-ready-vr-ar-virtual-reality-content-photo"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
